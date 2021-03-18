8431b79bb1ea469718507e73a04108ff155caf74
aa <aa@aa.com>
2021-03-19 00:01:09 +0800

    Converter seems to partially work

--- convert_to_pecan.py
+++ convert_to_pecan.py
@@ -4,7 +4,7 @@ import os
 from copy import deepcopy
 import sys
 import pprint
-pprint = pprint.PrettyPrinter(depth=6).pprint
+pprint = pprint.PrettyPrinter(depth=5).pprint
 
 aut_template = {
     'name': None,
@@ -41,45 +41,66 @@ def parse(filename, n):
                 curr_state = state
             else:
                 inp, to = list(map(lambda x: int(x), l.split('->')))
-                aut_to_read['transitions'][curr_state][inp] = to
+                aut_to_read['transitions'][curr_state][inp] = set({to})
     return aut_to_read
 
-# Generate a separate automata occupying states numbered between start_num~start_num+log2(num)+1,
-# that would recognize [lsd_bin(num)]0^ω2 as input
-# There is an optional final state to make concatenating more convenient
 
-
-def creat_recognizing(num, start_num=0, final_state=None, start_state=None):
-    aut_to_read = deepcopy(aut_template)
-    aut_to_read['name'] = f'recg{num}'
-    aut_to_read['alphabet'] = {0, 1, 2}
-
-    nums_encoded = list(reversed(bin(num)))[:-2]
-    # remove tailing zeros
+def lsd_bin(num):
+    nums_encoded = list(map(lambda x: int(x), reversed(bin(num)[2:])))
+    # nums_encoded = list(map(lambda x: (x), reversed(bin(num)[2:])))
     while len(nums_encoded) and nums_encoded[-1] == 0:
         nums_encoded = nums_encoded[:-1]
-    # additional states: start, 0^ω, end
-    aut_to_read['states'] = start_num + np.arange(len(nums_encoded)+2)
-    if final_state is not None:
-        aut_to_read['states'][-1] = final_state
-    if start_state is not None:
-        aut_to_read['states'][0] = start_state
-    aut_to_read['initial_state'] = 0
-    aut_to_read['accepting_states'].add(aut_to_read['states'][-1])
+    return nums_encoded
 
-    for i in range(len(nums_encoded)):
-        if i == len(nums_encoded)-1:  # last one
-            aut_to_read['transitions'][aut_to_read['states'][i]] = {
-                2: aut_to_read['states'][-1],
-                nums_encoded[i]: aut_to_read['states'][i+1]}
-        else:
-            aut_to_read['transitions'][aut_to_read['states'][i]] = {
-                nums_encoded[i]: aut_to_read['states'][i+1]}
-    aut_to_read['transitions'][aut_to_read['states'][-2]] = {
-        0: aut_to_read['states'][-2],
-        2: aut_to_read['states'][-1]
-    }
-    return aut_to_read
+# Generate a separate automata occupying states numbered between start_num~start_num+log2(num)+1,
+# that would recognize [lsd_bin(i)]0^ω2 for i=0..n as input
+
+
+def creat_recognizing(n, start_num=0, final_states=None, start_state=0):
+    aut = deepcopy(aut_template)
+    aut['name'] = f'recg{n}'
+    aut['alphabet'] = {0, 1, 2}
+
+    representations = list(map(lsd_bin, range(n)))
+    nlongest = max(map(len, representations))
+
+    aut['states'] = np.arange(n*(nlongest+2)+1)+start_num
+
+    aut['states'][-(n+1)] = start_state
+    if final_states is not None:
+        final_states[final_states == -
+                     1] = aut['states'][-n:][final_states == -1]
+        aut['states'][-n:] = final_states
+
+    for s in aut['states'][:-n]:
+        aut['transitions'][s] = {0: set(), 1: set(), 2: set()}
+    # i in 0..n
+    for i in range(n):
+        rep = representations[i]
+        for k in range(len(rep)):
+            st = k*n+i
+            inp = rep[k]
+            end = (k+1)*n+i
+            aut['transitions'][aut['states'][st]][inp].add(aut['states'][end])
+            # if k == len(rep)-1:  # last
+                # aut['transitions'][aut['states'][st]][2].add(final_states[i])
+        aut['transitions'][aut['states']
+                           [len(rep)*n+i]][0].add(aut['states'][len(rep)*n+i])
+        aut['transitions'][aut['states']
+                           [len(rep)*n+i]][2].add(final_states[i])
+
+    aut['transitions'][start_state][0] = set.union(*[dst[0] for src,
+                                                     dst in aut['transitions'].items() if src in aut['states'][:n]])
+    aut['transitions'][start_state][1] = set.union(*[dst[1] for src,
+                                                     dst in aut['transitions'].items() if src in aut['states'][:n]])
+    aut['transitions'][start_state][2] = set.union(*[dst[2] for src,
+                                                     dst in aut['transitions'].items() if src in aut['states'][:n]])
+    # remove empty sets
+    # aut['states'] = list(filter(lambda x: x in aut['transitions'] and len(
+    # aut['transitions'][x].items()), aut['states']))
+    aut['transitions'] = {
+        src: {inp: v for inp, v in dst.items() if len(v)} for src, dst in aut['transitions'].items()}
+    return aut
 
 
 def convert(filename, n):
@@ -87,31 +108,25 @@ def convert(filename, n):
     print('Read original automaton:')
     pprint(org_aut)
 
-    unoccupied_state_number = max(org_aut['states'])+100
+    unoccupied_state_number = max(org_aut['states'])+1
 
-    # reg1234 = creat_recognizing(1234, start_num=100)
+    # reg1234 = creat_recognizing(
+    # n, start_num=10000, start_state=-1, final_states=np.arange(n)+10)
     # print('Create automaton recognizing bin(1234)[2]:')
     # pprint(reg1234)
 
-    for src_state in org_aut['transitions'].keys():
-        for inp in org_aut['transitions'][src_state].keys():
-            dst_state = org_aut['transitions'][src_state][inp]
-            print(f'Bridging {src_state}-{inp}->{dst_state}:')
+    org_keys = list(org_aut['transitions'].keys())
+    for src_state in org_keys:
+        final_states = np.array([list(org_aut['transitions'][src_state][i])[
+            0] if i in org_aut['transitions'][src_state] else -1 for i in range(n)])
+        bridge_aut = creat_recognizing(
+            n, start_num=unoccupied_state_number, final_states=final_states, start_state=src_state)
+        pprint(bridge_aut)
 
-            bridge_aut = creat_recognizing(
-                inp, start_num=unoccupied_state_number, final_state=dst_state, start_state=src_state)
-            pprint(bridge_aut)
-
-            first_transition = bridge_aut['transitions'][src_state]
-            del bridge_aut['transitions'][src_state]
-
-            print("first transition: ")
-            pprint(first_transition)
-            org_aut['states'] |= set(bridge_aut['states'])
-            org_aut['transitions'].update(bridge_aut['transitions'])
-            break
-        break
-    pass
+        org_aut['states'] |= set(bridge_aut['states'])
+        org_aut['transitions'].update(bridge_aut['transitions'])
+        unoccupied_state_number = max(org_aut['states'])+1
+        # break
 
     print("Final bridged automaton:")
     pprint(org_aut)
